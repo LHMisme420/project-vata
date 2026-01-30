818c4333757bd0635f1285463779b6e525281a6d
LHMisme420 <lhmisme2011@gmail.com>
2026-01-30 12:57:22 -0500

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(
+    filename="vata.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def log(msg):
+    logging.info(msg)
